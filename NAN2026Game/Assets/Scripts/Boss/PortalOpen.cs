0de3803b4d2e40a06a27b03f29b334d907c6ded3
NoImpMe <hn00428@naver.com>
2026-08-10 03:01:12 +0900

    no message

--- NAN2026Game/Assets/Scripts/Boss/PortalOpen.cs
+++ NAN2026Game/Assets/Scripts/Boss/PortalOpen.cs
@@ -1,12 +1,21 @@
+using NAN2026;
 using UnityEngine;
 
 public class PortalOpen : MonoBehaviour
 {
-    [SerializeField] private GameObject portal; 
+    [SerializeField] private GameObject portal;
+    MidBoss_FireKnight tmp;
+    bool check;
     // Update is called once per frame
+    private void Awake()
+    {
+        tmp = gameObject.GetComponent<MidBoss_FireKnight>();
+        check = tmp.death;
+    }
     void Update()
     {
-        if (gameObject.IsDestroying())
+        check = tmp.death;
+        if (check)
         {
             portal.SetActive(true);
         }
